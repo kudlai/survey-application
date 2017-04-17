3335d64ce2b5b7cbe2c6a0a7e2d16706e13f8ecb
Ilya Kudlay <ikudlay@axmit.com>
2017-04-17 17:14:59 +0300

    remove versions

--- push.sh
+++ push.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-version=1
-
-docker tag ikudlay/survey-application ikudlay/survey-application:$version
-docker push ikudlay/survey-application:$version
+docker push ikudlay/survey-application
